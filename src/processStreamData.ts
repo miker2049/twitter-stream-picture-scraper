bb93db6aeac15384db3b569725275e840ef13f41
Mike Russo <russomichaelb@gmail.com>
2022-03-16 01:40:42 -0400

    Silly silly things

--- src/processStreamData.ts
+++ src/processStreamData.ts
@@ -5,7 +5,7 @@ export default function(data: any, tag: string): ILogObject | undefined{
     try {
         const parsed= JSON.parse(data)
         if(parsed.includes && parsed.includes.media[0].url != undefined ){
-            if (parsed.matching_rules.find((v)=>v.tag===tag)===undefined ? false : true) {
+            if (!( parsed.matching_rules.find((v)=>v.tag===tag)===undefined )) {
                 // console.log(parsed)
                 output = {url: parsed.includes.media[0].url, text:parsed.data.text, authorid: parsed.data.author_id, created_at: parsed.data.created_at}
             }
