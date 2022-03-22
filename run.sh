6295b491ac648468aa9ead0a1425426199dc8ac2
Mike Russo <russomichaelb@gmail.com>
2022-03-22 03:14:44 -0400

    Random start; cli args

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 workdir=$(mktemp -d)
-yarn run start -s true -c 500 -o $workdir"/"
+yarn run start -s true -c 1000 -o $workdir"/"
 ./mash_pics.sh $workdir $workdir/video.mp4
 #1000 at 24 = 42000ms
-python ./src-audio/make-song.py $(./get-random-song.sh) $workdir/audio.wav 21000 $workdir/video.mp4
+# workdir="/tmp/tmp.b7Yo1n0sw2"
+python ./src-audio/make-song.py "$(./get-random-song.sh)" $workdir/audio.wav 42000
 
 ffmpeg -i $workdir/audio.wav -i $workdir/video.mp4 -c:v copy -c:a aac -map 1:v:0 -map 0:a:0 $1
 rm -rf $workdir
--- src-audio/make-song.py
+++ src-audio/make-song.py
@@ -3,6 +3,7 @@ from pydub import AudioSegment, effects
 import os
 import sys
 import math
+from random import random
 
 input_raw = sys.argv[1]
 
@@ -14,16 +15,23 @@ ext = os.path.splitext(input_file)[1][1:]
 song = AudioSegment.from_file(input_file, format=ext)
 assert isinstance(song, AudioSegment)
 
-input_duration = song.duration_seconds
+input_duration = song.duration_seconds * 1000
 
 clip_duration = 240 # how long is the clip
 
 change_times = clip_duration * 16  # how often it changes over time
 change_amount = 1000 # how much it changes over time
 
-start = 30 * 1000 # where to do a random soon
-song_duration = 40000 # output dur
+song_duration = 42000 # output dur
 
+start = random() * (input_duration-song_duration)  # where to do a random soon
+
+outfile = "out.mp4"
+if(sys.argv[2]):
+    outfile = str(sys.argv[2])
+
+if(sys.argv[3]):
+    song_duration = int(sys.argv[3])
 
 n = 0
 i = 0
@@ -49,4 +57,4 @@ while(n<song_duration):
     n += clip_duration
     i += 1
 out = effects.normalize(out)
-out.export("mashup.mp3", format="mp3")
+out.export(outfile, format="wav")
