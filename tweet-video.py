8c2e5ff9fb447097d16d3e7cca966be6dfa36a78
Mike Russo <russomichaelb@gmail.com>
2022-03-23 02:27:13 -0400

    Commit to pipenv

--- tweet-video.py
+++ tweet-video.py
@@ -3,12 +3,8 @@ import os
 import sys
 import time
 
-import json
 import requests
 from requests_oauthlib import OAuth1
-from dotenv import load_dotenv
-
-load_dotenv()
 
 MEDIA_ENDPOINT_URL = 'https://upload.twitter.com/1.1/media/upload.json'
 POST_TWEET_URL = 'https://api.twitter.com/1.1/statuses/update.json'
