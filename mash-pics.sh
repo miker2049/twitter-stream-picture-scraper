cdfe852341db4a68f3d9cf84100955f2871b1932
Mike R <russomichaelb@gmail.com>
2022-05-08 13:30:53 -0400

    Roll your own color2alpha

--- mash-pics.sh
+++ mash-pics.sh
@@ -8,9 +8,28 @@ image_ontop(){
     $magick \
         $base \
         \( \
-        $new \
-        \( +clone -auto-threshold Triangle \) \
-        -compose dst-in -composite \
+            $new \
+            \( +clone -auto-threshold Triangle \) \
+            -compose dst-in -composite    \
+            \(                          \
+                -clone 0                \
+                -fill "#000000"         \
+                -colorize 100           \
+            \)                          \
+            \(                          \
+                -clone 0,1              \
+                -compose difference     \
+                -composite              \
+                -separate               \
+                +channel                \
+                -evaluate-sequence max  \
+                -auto-level             \
+            \)                        \
+            -delete 1                 \
+            -alpha off                \
+            -compose over             \
+            -compose copy_opacity     \
+            -composite                \
         \) \
         -compose src-over -composite \
         -modulate 100,100,100 \
