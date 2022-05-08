1e868ea18b0d8cd6a2c4694b3dd2b0df5916a4ce
Mike R <russomichaelb@gmail.com>
2022-05-08 14:06:22 -0400

    improved transparency

--- mash-pics.sh
+++ mash-pics.sh
@@ -11,28 +11,10 @@ image_ontop(){
             $new \
             \( +clone -auto-threshold Triangle \) \
             -compose dst-in -composite    \
-            \(                          \
-                -clone 0                \
-                -fill "#000000"         \
-                -colorize 100           \
-            \)                          \
-            \(                          \
-                -clone 0,1              \
-                -compose difference     \
-                -composite              \
-                -separate               \
-                +channel                \
-                -evaluate-sequence max  \
-                -auto-level             \
-            \)                        \
-            -delete 1                 \
-            -alpha off                \
-            -compose over             \
-            -compose copy_opacity     \
-            -composite                \
+            -fuzz 20% \
+            -transparent white \
         \) \
         -compose src-over -composite \
-        -modulate 100,100,100 \
         $out
 }
 
